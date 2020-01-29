c95102d626f17901f4549e8d4bad6f14f16c5f1b
Thomas Tippold <thomas.tippold@gmail.com>
2020-01-29 23:58:08 +0100

    Add navigation and basic styling

--- Application/view/header.php
+++ Application/view/header.php
@@ -4,8 +4,20 @@
     <meta charset="ISO-8859-1">
     <meta name="viewport" content="width=device-witch, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="<?= $this->css_path ?>">
+    <script src="https://kit.fontawesome.com/2e91dbc0a1.js" crossorigin="anonymous"></script>
 
     <title><?= $this->page_title; ?></title>
 </head>
 
 <body>
+<div id="sidebar">
+    <nav>
+        <ul>
+            <a href="/sportradar/"><li><i class="far fa-calendar-alt fa-3x"></i></li></a>
+            <a href="/sportradar/event/add/"><li><i class="far fa-calendar-plus fa-3x"></i></li></a>
+            <a><li><i class="fas fa-cog fa-2x"></i></li></a>
+        </ul>
+    </nav>
+</div>
+
+<main id="contentcontainer">
